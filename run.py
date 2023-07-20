d4fc5a9ee63423bf5d86a7629726e01de9daf28b
Rodney-D-Dev <>
2023-07-20 21:58:56 +0000

    Add Main game board class

--- run.py
+++ run.py
@@ -1,7 +1,43 @@
 from random import randint 
 
 class Game_board:
+    """
+    Main Game board class. Sets board sise, number of ships, the players name and board type 
+    has methos for printing board, guesses, adding shhips and checking all ships have sunk.
+    """
+    def __init__(self,size,num_ships,name,type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.ships = []
+        self.guesses = []
+    
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+    
+    def guess(self, x, y):
+        self.guesses.append((x,y))
+        self.board[x][y] = "x"
 
+        if(x,y) in self.ships:
+            self.board[x][y] = "*"
+            return "Hit"
+        else:
+            return "Miss"
+
+    def add_ship(self,x,y, type = "computer"):
+        if len(self.ships) >= self.num_ships:
+            print("Error: you cannot add any more ships!")
+        else:
+            self.ships.append((x,y))
+            if self.type == "player":
+                self.board[x][y] = "@"
+    def all_ships_sunk(self):
+        return all(ship in self.guesses for ship in self.ships)
+        
 def random_point(size):
     pass
 
