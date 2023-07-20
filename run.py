9ab5a8b2d83bdaf774971303971e5ba54b3a0979
Rodney-D-Dev <>
2023-07-20 21:41:51 +0000

    Add class and methos for game

---
+++ run.py
@@ -0,0 +1,18 @@
+from random import randint 
+
+class Game_board:
+
+def random_point(size):
+    pass
+
+def valid_coordinates():
+    pass
+
+def populate_board():
+    pass
+
+def make_guess():
+    pass
+
+def play_game():
+    pass
